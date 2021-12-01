1fb87d64f8fcd8d0b585652b0e846c3e0a86b652
Aleksandra Bošković <sandraboskovic@hotmail.com>
2021-12-01 05:25:05 +0100

    update/remove mongoose options

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect('mongodb://localhost/library-project', {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+  .connect('mongodb://localhost/library-project')
   .then(x =>
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   )
